d0f1a54f14932090a3271e765f7d158ba9c4242f
makhariaaayush <63086244+makhariaaayush@users.noreply.github.com>
2021-09-18 10:46:21 -0500

    test case 3 done

--- src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
+++ src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
@@ -1,6 +1,7 @@
 package edu.uic.cs474.f21.a1.solution;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import edu.uic.cs474.f21.a1.DynamicDispatchExplainer;
 
@@ -11,39 +12,40 @@ import java.util.Set;
 public class A1Solution implements DynamicDispatchExplainer {
     @Override
     public Set<String> explain(Map<String, ClassOrInterfaceDeclaration> classes, String receiverType, String methodName, String... argumentTypes) {
+
         Set<String> ret = new HashSet<>();
+        boolean flag = false;
+
         ClassOrInterfaceDeclaration d = classes.get(receiverType);
 
-        if (!d.getMethodsByName(methodName).isEmpty()) {
-            ret.add(d.getName().asString());
+        while (d.getMethodsBySignature(methodName,argumentTypes).isEmpty()) {
+            ClassOrInterfaceType d1 = d.getExtendedTypes().get(0);
+            d = classes.get(d1.getName().asString());
+            flag = true;
         }
-        else
-        {
-            while(!d.getExtendedTypes().isEmpty())
-            {
-                ClassOrInterfaceType d1 = d.getExtendedTypes().get(0);
-                d = classes.get(d1.getName().asString());
+
+        if (!d.getMethodsBySignature(methodName,argumentTypes).isEmpty()) {
+            MethodDeclaration a = d.getMethodsBySignature(methodName, argumentTypes).get(0);
+//            To Check Method "a" is Static or Private
+            if (a.isStatic() || a.isPrivate()) {
+                if (!flag) {
+                    ret.add(d.getName().asString());
+                }
+                return ret;
             }
+//            To Check is Method "a" is Abstract
+            if (a.isAbstract()) {
+                return ret;
+            }
+//            Return a value if none
             ret.add(d.getName().asString());
         }
-//            ClassOrInterfaceDeclaration temp = new ClassOrInterfaceDeclaration();
-//            if(!d.getExtendedTypes().isEmpty())
-//            {
-//                ClassOrInterfaceType a = d.getExtendedTypes().get(0);
-//                ClassOrInterfaceDeclaration temp = classes.get(a.getName().asString());
-//                if(!temp.getExtendedTypes().isEmpty())
-//                {
-//                    ClassOrInterfaceType a1 = temp.getExtendedTypes().get(0);
-//                    ClassOrInterfaceDeclaration temp1 = classes.get(a1.getName().asString());
-//                    ret.add(temp1.getName().asString());
-//                }
-//                ret.add(temp.getName().asString());
-//            }
-//        }
         return ret;
     }
-
 }
+
+
+
 
 
 
